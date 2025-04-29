ff63996729f5fcacc4f56776805241eba185e8b9
KoalaGM <arthurhsassis@hotmail.com>
2025-04-29 20:53:41 -0300

    Adição da atividade 01

---
+++ Aula-7/atividade01.py
@@ -0,0 +1,16 @@
+def calcular_valores(num):
+    dobro = num * 2
+    triplo = num * 3
+    quadrado = num ** 2
+    return dobro, triplo, quadrado
+
+
+numero = float(input("Digite o valor do lado do adesivo em cm: "))
+
+
+dobro, triplo, quadrado = calcular_valores(numero)
+
+
+print(f"O dobro do número é: {dobro} cm")
+print(f"O triplo do número é: {triplo} cm")
+print(f"O quadrado do número é: {quadrado} cm²")
